431229171dab75dd400b3442cd46399a2c027c91
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2022-09-20 15:20:06 +0200

    Nethermind setup post merge

---
+++ build/reload-certs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+while true; do
+    date > /tmp/reload-certs.txt
+
+    echo "Check for updated certificates"
+
+    md5sumbefore=$(md5sum "/etc/nginx/certs/my.ava.do.crt")
+
+    wget -q -O /etc/nginx/certs/my.ava.do.crt http://iso.ava.do/my.ava.do.crt
+    wget -q -O /etc/nginx/certs/my.ava.do.key http://iso.ava.do/my.ava.do.key
+
+    md5sumafter=$(md5sum "/etc/nginx/certs/my.ava.do.crt")
+
+    if [ "$md5sumbefore" != "$md5sumafter" ]; then
+        if [ -e /var/run/nginx/nginx.pid ]; then
+            echo "Reload nginx"
+            nginx -s reload
+        fi
+    fi
+
+    #sleep one day
+    sleep 86400
+done
